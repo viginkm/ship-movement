d9118ed94bad3ebffade2b2bb6907d487ccf48e7
vigin <viginkm@rediffmail.com>
2025-04-01 12:12:45 +0530

    base of  application

---
+++ ship-shink/src/main/java/com/ship/probe/ShipShinkApplication.java
@@ -0,0 +1,13 @@
+package com.ship.probe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipShinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipShinkApplication.class, args);
+	}
+
+}
---
+++ ship-shink/src/test/java/com/ship/probe/ShipShinkApplicationTests.java
@@ -0,0 +1,11 @@
+package com.ship.probe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipShinkApplicationTests {
+
+	
+
+}
